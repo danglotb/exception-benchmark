6e19bafe0a53535baa50043db0a1d9de14391e74
danglotb <bdanglot@gmail.com>
2017-07-04 14:37:46 +0200

    update python scripts

--- bugs/mayocat_231/main.py
+++ bugs/mayocat_231/main.py
@@ -1,2 +1,12 @@
 #!/bin/python
 
+import requests
+
+s = requests.Session()
+
+r = s.get("http://172.17.0.3:8080")
+print(r.status_code)
+r = s.post("http://172.17.0.3:8080/cart/add", {"product": "product"})
+print(r.status_code)
+r = s.get("http://172.17.0.3:8080")
+print(r.status_code)
--- bugs/mayocat_231_patch/main.py
+++ bugs/mayocat_231_patch/main.py
@@ -4,9 +4,9 @@ import requests
 
 s = requests.Session()
 
-r = s.get("http://172.17.0.1:8080")
+r = s.get("http://172.17.0.2:8080")
 print(r.status_code)
-r = s.post("http://172.17.0.1:8080/cart/add", {"product": "product"})
+r = s.post("http://172.17.0.2:8080/cart/add", {"product": "product"})
 print(r.status_code)
-r = s.get("http://172.17.0.1:8080")
+r = s.get("http://172.17.0.2:8080")
 print(r.status_code)
